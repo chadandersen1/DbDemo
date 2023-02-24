0621421658010de83fe6f7abd6dd9ef833debb28
Chad <candycanekid77@gmail.com>
2023-02-24 11:47:42 -0700

    add catalog thing

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        table, tr, td {
+        table, tr, th, td {
             border: 1px solid black;
             border-collapse: collapse;
             padding: 1%;
@@ -15,34 +15,33 @@
 </head>
 <body>
 
-    <?php
+    <!-- // echo "<p>Start of php</p>";
 
-    echo "<p>Start of php</p>";
+    // $servername = "localhost";
+    // $username = "steverq1_chad";
+    // $password = "Csci213+#003";
+    // $dbname = "steverq1_chad";
 
-    $servername = "localhost";
-    $username = "steverq1_chad";
-    $password = "Csci213+#003";
-    $dbname = "steverq1_chad";
+    // $db_conn = new mysqli($servername, $username, $password, $dbname);
 
-    $db_conn = new mysqli($servername, $username, $password, $dbname);
+    // if ($db_conn->connect_error) {
+    //     echo "Database connection failed: " . $db_conn->connect_error;
+    // } else {
+    //     echo "Success<br />";
+    // }
 
-    if ($db_conn->connect_error) {
-        echo "Database connection failed: " . $db_conn->connect_error;
-    } else {
-        echo "Success<br />";
-    }
+    // $result = $db_conn->query("SELECT * FROM customer;");
 
-    $result = $db_conn->query("SELECT * FROM customer;");
+    // echo "<table><tr><th>Customer ID</th><th>Customer first name</th><th>Customer last name</th><th>Customer Email</th></tr>";
+    // while ($row = $result->fetch_assoc()) {
+    //     $cust_id = $row['cust_id'];
+    //     $cust_fname = $row['cust_fname'];
+    //     $cust_lname = $row['cust_lname'];
+    //     $cust_email = $row['cust_email'];
+    //     echo "<tr><td>$cust_id</td><td>$cust_fname</td><td>$cust_lname</td><td>$cust_email</td></tr>";
+    // } -->
 
-    echo "<table><tr><th>Customer ID</th><th>Customer first name</th><th>Customer last name</th><th>Customer Email</th></tr>";
-    while ($row = $result->fetch_assoc()) {
-        $cust_id = $row['cust_id'];
-        $cust_fname = $row['cust_fname'];
-        $cust_lname = $row['cust_lname'];
-        $cust_email = $row['cust_email'];
-        echo "<tr><td>$cust_id</td><td>$cust_fname</td><td>$cust_lname</td><td>$cust_email</td></tr>";
-    }
-        ?>
+    <a href="addCatalogItem-lab.php">Add a catalog item</a>
         
 
 </body>
